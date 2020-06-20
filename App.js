69bdb27446894d1c1e3cd82635d24fce7be9c6f3
Jason Brown <browniefed@gmail.com>
2020-06-20 13:26:35 -0700

    Repro

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Linking } from "expo";
+import { View, Text, Button } from "react-native";
+import { useLinkProps } from "@react-navigation/native";
 
-export default function App() {
+const Stack = createStackNavigator();
+const prefix = Linking.makeUrl("/");
+
+const DetailsScreen = ({ navigation, route }) => {
+  console.log(route.params);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
     </View>
+  );
+};
+
+const HomeScreen = () => {
+  const { onPress, ...props } = useLinkProps({
+    to: "/details?id=123",
+  });
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button {...props} onPress={onPress} title="Go To Jane" />
+    </View>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer
+      linking={{
+        prefixes: [prefix],
+        config: {
+          Home: "home",
+          Details: {
+            path: "details/:id",
+            // path: "details/:id?",
+          },
+        },
+      }}
+    >
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
